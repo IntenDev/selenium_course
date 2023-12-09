35f4ce10d3d498fed456a7fd01385349238643f5
Ilya <ilya@kostech.ru>
2023-12-09 19:58:12 +0300

    execute js elements

---
+++ section2/dir2_lesson2_step6_execute_js.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import math
+import time
+try:
+    def calc(x):
+      return str(math.log(abs(12*math.sin(int(x)))))
+    browser = webdriver.Chrome()
+    url = "https://SunInJuly.github.io/execute_script.html"
+    browser.get(url)
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "#answer")
+    input1.send_keys(y)
+
+    option1 = browser.find_element(By.CSS_SELECTOR, "#robotCheckbox")
+    option1.click()
+
+
+    option2 = browser.find_element(By.CSS_SELECTOR, "#robotsRule")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", option2)
+    option2.click()
+
+    button = browser.find_element(By.TAG_NAME, "button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+finally:
+    time.sleep(10)
+    browser.quit()
+
+
